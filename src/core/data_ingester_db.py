f80413f6ec3bfde89ee5fdb0fcd9f0cb06609fe4
Guilherme Palazzo <palazzoeng1@gmail.com>
2020-10-20 11:54:29 -0300

    encapsulating code of db_ingester into functions

--- src/core/data_ingester_db.py
+++ src/core/data_ingester_db.py
@@ -2,30 +2,45 @@ from pathlib import Path
 from src.clients.db_client import DBClient
 import glob
 import pandas as pd
+from typing import List
 
 
-desktop_path = Path(__file__).resolve().parents[5]
-files_local_storage_path = f"{desktop_path}/personal_app_financial_data_producer/storage/financial_data"
-
-files_list = glob.glob(f"{files_local_storage_path}/*.json")
-
-def _filter_out_file_names(file_name):
+def _filter_out_file_names(file_name: str) -> bool:
     return "expenses" not in file_name
 
-files_list = filter(_filter_out_file_names, files_list)
-files_list = list(files_list)
 
-df = pd.DataFrame()
+def _identify_number_signal_and_cast_float(series: pd.Series) -> pd.Series:
+    # identify if the number is positive or negative
+    series = series.apply(lambda x: -x if x[-1] == "D" else x)
+    # removing letter at the end and casting to float
+    return series.apply(lambda x: float(x[:-1]))
+     
 
-for file in files_list:
-    df_aux = pd.read_json(file)
-    df = df.append(df_aux, ignore_index=True)
+def _get_files_list() -> List[str]:
+    desktop_path = Path(__file__).resolve().parents[5]
+    files_local_storage_path = f"{desktop_path}/personal_app_financial_data_producer/storage/financial_data"
 
-# identify if the number is positive or negative
-df.balance_value = df.balance_value.apply(lambda x: -x if x[-1] == "D" else x)
-# removing letter at the end and casting to float
-df.balance_value = df.balance_value.apply(lambda x: float(x[:-1]))
+    files_list = glob.glob(f"{files_local_storage_path}/*.json")
 
+    files_list = filter(_filter_out_file_names, files_list)
+    
+    return list(files_list)
+
+
+def _generate_and_append_dfs(files_list: List[str],
+                             df: pd.DataFrame) -> pd.DataFrame:
+    
+    for file in files_list:
+        df_aux = pd.read_json(file)
+        df = df.append(df_aux, ignore_index=True)
+    
+    return df
+
+files_list = _get_files_list()
+df = _generate_and_append_dfs(files_list=files_list,
+                              df=pd.DataFrame())
+
+df.balance_value = _identify_number_signal_and_cast_float(series=df.balance_value)
 db = DBClient(db_name="financial")
 
 df.to_sql(
